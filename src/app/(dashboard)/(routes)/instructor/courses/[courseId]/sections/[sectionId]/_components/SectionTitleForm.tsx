574a75096fbbf155f729bcd0e3b28673c48952f9
Bermuda <olasunkanmiboluwatife110@gmail.com>
2023-12-06 08:32:01 +0100

    (feat): section title form UI

---
+++ src/app/(dashboard)/(routes)/instructor/courses/[courseId]/sections/[sectionId]/_components/SectionTitleForm.tsx
@@ -0,0 +1,124 @@
+'use client'
+
+import * as z from 'zod'
+import axios from 'axios'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useForm } from 'react-hook-form'
+import { Pencil } from 'lucide-react'
+import { useState } from 'react'
+import toast from 'react-hot-toast'
+import { useRouter } from 'next/navigation'
+
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormMessage,
+} from '@/components/ui/form'
+import { Input } from '@/components/ui/input'
+import { Button } from '@/components/ui/button'
+import { useToast } from '@/components/ui/use-toast'
+
+interface ChapterTitleFormProps {
+  initialData: {
+    title: string
+  }
+  courseId: string
+  sectionId: string
+}
+
+const formSchema = z.object({
+  title: z.string().min(1),
+})
+
+export default function ChapterTitleForm({
+  initialData,
+  courseId,
+  sectionId,
+}: ChapterTitleFormProps) {
+  const [isEditing, setIsEditing] = useState(false)
+
+  const toggleEdit = () => setIsEditing((current) => !current)
+
+  const router = useRouter()
+  const { toast } = useToast()
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: initialData,
+  })
+
+  const { isSubmitting, isValid } = form.formState
+
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    try {
+      await axios.patch(
+        `/api/courses/${courseId}/chapters/${sectionId}`,
+        values
+      )
+      toast({
+        title: 'Success',
+        description: 'Your course section was successfully updated',
+        variant: 'default',
+      })
+      toggleEdit()
+      router.refresh()
+    } catch {
+      toast({
+        title: 'Error',
+        description: 'Something went wrong, please try again later',
+        variant: 'destructive',
+      })
+    }
+  }
+
+  return (
+    <div className="mt-6 border bg-slate-100 rounded-md p-4">
+      <div className="font-medium flex items-center justify-between">
+        Chapter title
+        <Button onClick={toggleEdit} variant="ghost">
+          {isEditing ? (
+            <>Cancel</>
+          ) : (
+            <>
+              <Pencil className="h-4 w-4 mr-2" />
+              Edit title
+            </>
+          )}
+        </Button>
+      </div>
+      {!isEditing && <p className="text-sm mt-2">{initialData.title}</p>}
+      {isEditing && (
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(onSubmit)}
+            className="space-y-4 mt-4"
+          >
+            <FormField
+              control={form.control}
+              name="title"
+              render={({ field }) => (
+                <FormItem>
+                  <FormControl>
+                    <Input
+                      disabled={isSubmitting}
+                      placeholder="e.g. 'Introduction to the course'"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <div className="flex items-center gap-x-2">
+              <Button disabled={!isValid || isSubmitting} type="submit">
+                Save
+              </Button>
+            </div>
+          </form>
+        </Form>
+      )}
+    </div>
+  )
+}
