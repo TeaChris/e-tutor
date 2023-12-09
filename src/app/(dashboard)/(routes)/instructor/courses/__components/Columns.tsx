a431f2cd7bc3cd32c043a9f83c5968e22c6cb308
Bermuda <olasunkanmiboluwatife110@gmail.com>
2023-12-09 10:58:24 +0100

    (feat): filter course && edit course on table

--- src/app/(dashboard)/(routes)/instructor/courses/__components/Columns.tsx
+++ src/app/(dashboard)/(routes)/instructor/courses/__components/Columns.tsx
@@ -1,10 +1,19 @@
 'use client'
 
-import { ArrowUpDown, MoreHorizontal } from 'lucide-react'
+import { ArrowUpDown, MoreHorizontal, Pencil } from 'lucide-react'
 import { Course } from '@prisma/client'
 import { ColumnDef } from '@tanstack/react-table'
 
 import { Button } from '@/components/ui/button'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu'
+import Link from 'next/link'
+import { Badge } from '@/components/ui/badge'
+import { cn } from '@/lib/utils'
 
 export const columns: ColumnDef<Course>[] = [
   {
@@ -23,7 +32,7 @@ export const columns: ColumnDef<Course>[] = [
   },
   {
     accessorKey: 'price',
-     header: ({ column }) => {
+    header: ({ column }) => {
       return (
         <Button
           variant={'ghost'}
@@ -34,10 +43,19 @@ export const columns: ColumnDef<Course>[] = [
         </Button>
       )
     },
+    cell: ({ row }) => {
+      const price = parseFloat(row.getValue('price') || '0')
+      const formatted = new Intl.NumberFormat('en-US', {
+        style: 'currency',
+        currency: 'USD',
+      }).format(price)
+
+      return <div>{formatted}</div>
+    },
   },
   {
     accessorKey: 'isPublished',
-     header: ({ column }) => {
+    header: ({ column }) => {
       return (
         <Button
           variant={'ghost'}
@@ -46,6 +64,40 @@ export const columns: ColumnDef<Course>[] = [
           Published
           <ArrowUpDown className="ml-2 h-4 w-4" />
         </Button>
+      )
+    },
+    cell: ({ row }) => {
+      const isPublished = row.getValue('isPublished') || false
+
+      return (
+        <Badge className={cn('bg-slate-500', isPublished && 'bg-sky-700')}>
+          {isPublished ? 'Published' : 'Draft'}
+        </Badge>
+      )
+    },
+  },
+  {
+    id: 'actions',
+    cell: ({ row }) => {
+      const { id } = row.original
+
+      return (
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant={'ghost'} className="h-4 w-8 p-0">
+              <span className="sr-only"></span>
+              <MoreHorizontal className="h-4 w-4" />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <Link href={`/instructor/courses/${id}`}>
+              <DropdownMenuItem>
+                <Pencil className="h-4 w-4 mr-2" />
+                Edit
+              </DropdownMenuItem>
+            </Link>
+          </DropdownMenuContent>
+        </DropdownMenu>
       )
     },
   },
