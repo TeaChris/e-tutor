20f702f7acdb6cd57b2a0826ba91cb896fc854e3
Bermuda <olasunkanmiboluwatife110@gmail.com>
2023-12-09 09:49:58 +0100

    (build): added packages for table)

--- src/app/(dashboard)/(routes)/instructor/courses/page.tsx
+++ src/app/(dashboard)/(routes)/instructor/courses/page.tsx
@@ -1,12 +1,30 @@
 import { Button } from '@/components/ui/button'
 import Link from 'next/link'
+import { DataTable } from './__components/DataTable'
+import { columns } from './__components/Columns'
+import { auth } from '@clerk/nextjs'
+import { redirect } from 'next/navigation'
+import { db } from '@/lib/db'
 
-export default function CoursesPage() {
+export default async function CoursesPage() {
+  const { userId } = auth()
+
+  if (!userId) {
+    return redirect('/')
+  }
+
+  const courses = await db.course.findMany({
+    where: {
+      userId,
+    },
+    orderBy: {
+      createdAt: 'desc',
+    },
+  })
+
   return (
     <div className="p-6">
-      <Link href="/instructor/create">
-        <Button>New Course</Button>
-      </Link>
+      <DataTable columns={columns} data={courses} />
     </div>
   )
 }
