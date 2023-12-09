5d2cef43a1ffb9b094b4cbc3f02c0d7290a339ae
Bermuda <olasunkanmiboluwatife110@gmail.com>
2023-12-09 13:44:28 +0100

    (feat): filter functionality and placing search itmens in the url

--- src/app/(students)/(routes)/browse/_components/Categories.tsx
+++ src/app/(students)/(routes)/browse/_components/Categories.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import { Category } from '@prisma/client'
 import {
   FcEngineering,
@@ -22,24 +24,31 @@ interface CategoriesProps {
 }
 
 const iconMap: Record<Category['name'], IconType> = {
-  "Music": FcMusic,
-  "Photography": FcOldTimeCamera,
-  "Fitness": FcSportsMode,
-  "Accounting": FcSalesPerformance,
+  Music: FcMusic,
+  Photography: FcOldTimeCamera,
+  Fitness: FcSportsMode,
+  Accounting: FcSalesPerformance,
   'Computer Science': FcMultipleDevices,
-  "Filming": FcFilmReel,
-  "Engineering": FcEngineering,
-  "Business": FcAcceptDatabase,
-  "Health & Fitness": FcAdvertising,
-  "Marketing": FcAndroidOs,
-  "Life Style": FcAnswers,
-  "Design": FcAlphabeticalSortingZa,
+  Filming: FcFilmReel,
+  Engineering: FcEngineering,
+  Business: FcAcceptDatabase,
+  'Health & Fitness': FcAdvertising,
+  Marketing: FcAndroidOs,
+  'Life Style': FcAnswers,
+  Design: FcAlphabeticalSortingZa,
 }
 
 export default function Categories({ items }: CategoriesProps) {
-  return <div className="flex items-center gap-x-2 overflow-x-auto pb-2">
-    {items.map((item)=>(
-        <CategoryItem key={item.id} label={item.name} icon={iconMap[item.name]} value={item.id}/>
-    ))}
-  </div>
+  return (
+    <div className="flex items-center gap-x-2 overflow-x-auto pb-2">
+      {items.map((item) => (
+        <CategoryItem
+          key={item.id}
+          label={item.name}
+          icon={iconMap[item.name]}
+          value={item.id}
+        />
+      ))}
+    </div>
+  )
 }
--- src/app/(students)/(routes)/browse/_components/CategoryItem.tsx
+++ src/app/(students)/(routes)/browse/_components/CategoryItem.tsx
@@ -1,4 +1,8 @@
+'use client'
+
 import { cn } from '@/lib/utils'
+import qs from 'query-string'
+import { usePathname, useRouter, useSearchParams } from 'next/navigation'
 import { IconType } from 'react-icons'
 
 interface CategoryItemProps {
@@ -12,11 +16,36 @@ export default function CategoryItem({
   icon: Icon,
   value,
 }: CategoryItemProps) {
+  const pathname = usePathname()
+  const router = useRouter()
+  const searchParams = useSearchParams()
+
+  const currentCategoryId = searchParams.get('categoryId')
+  const currentTitle = searchParams.get('title')
+
+  const isSelected = currentCategoryId === value
+
+  const onClick = () => {
+    const url = qs.stringifyUrl(
+      {
+        url: pathname,
+        query: {
+          title: currentTitle,
+          categoryId: isSelected ? null : value,
+        },
+      },
+      { skipNull: true, skipEmptyString: true }
+    )
+    router.push(url)
+  }
   return (
     <button
+      onClick={onClick}
       className={cn(
-        'py-2 px-3 text-sm border border-slate-200 rounded-full flex items-center gap-x-1 hover:border-orange-700 transition'
+        'py-2 px-3 text-sm border border-slate-200 rounded-full flex items-center gap-x-1 hover:border-orange-700 transition',
+        isSelected && 'border-orange-700 bg-orange-200/20 text-orange-800'
       )}
+      type="button"
     >
       {Icon && <Icon size={20} />}
       <div className="truncate">{label}</div>
