83fc885020d431f8c51377748c9a65a67ef521aa
Bermuda <olasunkanmiboluwatife110@gmail.com>
2023-12-09 09:52:46 +0100

    (feat): added table columns

---
+++ src/app/(dashboard)/(routes)/instructor/courses/__components/Columns.tsx
@@ -0,0 +1,52 @@
+'use client'
+
+import { ArrowUpDown, MoreHorizontal } from 'lucide-react'
+import { Course } from '@prisma/client'
+import { ColumnDef } from '@tanstack/react-table'
+
+import { Button } from '@/components/ui/button'
+
+export const columns: ColumnDef<Course>[] = [
+  {
+    accessorKey: 'title',
+    header: ({ column }) => {
+      return (
+        <Button
+          variant={'ghost'}
+          onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
+        >
+          Title
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      )
+    },
+  },
+  {
+    accessorKey: 'price',
+     header: ({ column }) => {
+      return (
+        <Button
+          variant={'ghost'}
+          onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
+        >
+          Price
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      )
+    },
+  },
+  {
+    accessorKey: 'isPublished',
+     header: ({ column }) => {
+      return (
+        <Button
+          variant={'ghost'}
+          onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
+        >
+          Published
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      )
+    },
+  },
+]
