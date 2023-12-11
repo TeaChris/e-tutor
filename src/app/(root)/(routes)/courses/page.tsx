134795bb9332631e6e3e549859aeceed37276e5f
Bermuda <olasunkanmiboluwatife110@gmail.com>
2023-12-11 09:21:05 +0100

    (feat): added course card to display courses

--- src/app/(root)/(routes)/courses/page.tsx
+++ src/app/(root)/(routes)/courses/page.tsx
@@ -1,3 +1,3 @@
 export default function About() {
-  return <div>AboutPage</div>
+  return <div>CoursesPage</div>
 }
--- src/app/(students)/(routes)/browse/page.tsx
+++ src/app/(students)/(routes)/browse/page.tsx
@@ -1,10 +1,10 @@
 import { db } from '@/lib/db'
 import Categories from './_components/Categories'
 import SearchInput from '../../_components/SearchInput'
-// import { getCourses } from '@/actions/get-courses'
+import { getCourses } from '@/actions/get-courses'
 import { auth } from '@clerk/nextjs'
 import { redirect } from 'next/navigation'
-// import CoursesList from './_components/CoursesList'
+import CoursesList from './_components/CoursesList'
 
 interface SearchPageProps {
   searchParams: { title: string; categoryId: string }
@@ -23,19 +23,19 @@ export default async function BrowsePage({ searchParams }: SearchPageProps) {
     },
   })
 
-  // const courses = await getCourses({
-  //   userId,
-  //   ...searchParams,
-  // })
+  const courses = await getCourses({
+    userId,
+    ...searchParams,
+  })
   return (
     <>
       <div className="px-6 pt-6 md:hidden md:mb-0 block">
         <SearchInput />
       </div>
-      <div className="p-6">
+      <div className="p-6 space-y-4">
         <Categories items={categories} />
 
-        {/* <CoursesList items={courses} /> */}
+        <CoursesList items={courses} />
       </div>
     </>
   )
