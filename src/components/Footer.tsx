d42e68727000abbe7dbd19716e529d4553b7e5c9
Bermuda <olasunkanmiboluwatife110@gmail.com>
2023-12-12 09:45:11 +0100

    (style): removed top-margin from footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import MaxWidthWrapper from './MaxWidthWrapper'
 
 export default function Footer() {
   return (
-    <footer className="mt-20 w-full h-28">
+    <footer className="mt-0 w-full h-28">
       <MaxWidthWrapper className="px-52 border-t-2 border-[#FD8E1F] flex items-center py-4 justify-between">
         <Image src="/icons/logo.svg" alt="logo" width={120} height={120} />
         <h2 className="text-2xl font-bold">Bermuda</h2>
