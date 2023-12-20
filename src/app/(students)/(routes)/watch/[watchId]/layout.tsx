e3d19dfce33bba2e3168a9f8f7a7b3223133db3c
Bermuda <olasunkanmiboluwatife110@gmail.com>
2023-12-20 13:07:44 +0100

    (feat): added layout && watch page to student route

---
+++ src/app/(students)/(routes)/watch/[watchId]/layout.tsx
@@ -0,0 +1,62 @@
+import { auth } from '@clerk/nextjs'
+import { redirect } from 'next/navigation'
+
+import { db } from '@/lib/db'
+import { getProgress } from '@/actions/get-progress'
+import CourseNavbar from './_components/CourseNavbar'
+import CourseSidebar from './_components/CourseSidebar'
+
+export default async function WatchLayout({
+  children,
+  params,
+}: {
+  children: React.ReactNode
+  params: { watchId: string }
+}) {
+  const { userId } = auth()
+
+  if (!userId) {
+    return redirect('/')
+  }
+
+  const course = await db.course.findUnique({
+    where: {
+      id: params.watchId,
+    },
+    include: {
+      sections: {
+        where: {
+          isPublished: true,
+        },
+        include: {
+          userProgress: {
+            where: {
+              userId,
+            },
+          },
+        },
+        orderBy: {
+          position: 'asc',
+        },
+      },
+    },
+  })
+
+  if (!course) {
+    return redirect('/')
+  }
+
+  const progressCount = await getProgress(userId, course.id)
+
+  return (
+    <div className="h-full">
+      <div className="h-[80px] md:pl-80 fixed inset-y-0 w-full z-50">
+        <CourseNavbar course={course} progressCount={progressCount} />
+      </div>
+      <div className="hidden md:flex h-full w-80 flex-col fixed inset-y-0 z-50">
+        <CourseSidebar course={course} progressCount={progressCount} />
+      </div>
+      <main className="md:pl-80 pt-[80px] h-full">{children}</main>
+    </div>
+  )
+}
