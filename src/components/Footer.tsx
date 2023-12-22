f0ef6e4a89e612e1fe6ca367b1255db57b995c80
Bermuda <olasunkanmiboluwatife110@gmail.com>
2023-12-22 12:05:34 +0100

    (style): footer responsiveness

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ import MaxWidthWrapper from './MaxWidthWrapper'
 export default function Footer() {
   return (
     <footer className="mt-0 w-full h-28">
-      <MaxWidthWrapper className="px-52 border-t-2 border-[#FD8E1F] flex items-center py-4 justify-between">
+      <MaxWidthWrapper className="lg:px-52 border-t-2 border-[#FD8E1F] flex items-center py-4 justify-between mt-4">
         <Image src="/icons/logo.svg" alt="logo" width={120} height={120} />
         <h2 className="text-2xl font-bold">Bermuda</h2>
       </MaxWidthWrapper>
