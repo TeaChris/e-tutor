d9e62f446509c666b3ccdfbe0bfaccebef48c8a0
Bermuda <olasunkanmiboluwatife110@gmail.com>
2023-12-25 16:53:04 +0100

    (refactor): changed watchId nav to be consistent

--- src/app/(students)/(routes)/watch/[watchId]/_components/CourseSidebarItem.tsx
+++ src/app/(students)/(routes)/watch/[watchId]/_components/CourseSidebarItem.tsx
@@ -36,7 +36,7 @@ export default function CourseSidebarItem({
       className={cn(
         'flex items-center gap-x-2 text-slate-500 text-sm font-[500] pl-6 transition-all hover:text-slate-600 hover:bg-slate-300/20',
         isActive &&
-          'text-slate-700 bg-slate-200/20 hover:bg-slate-200/20 hover:text-slate-700',
+          'bg-[#FF6636] text-white hover:bg-orange-700 hover:text-white',
         isCompleted && 'text-emerald-700 hover:text-emerald-700',
         isCompleted && isActive && 'bg-emerald-200/20'
       )}
