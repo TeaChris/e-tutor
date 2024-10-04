9563c8918665521cf487f43254059f477e632ae5
bermuda <olasunkanmiboluwatife110@gmail.com>
2024-10-04 17:51:01 +0100

    chore: updated import

--- src/components/ui/progress.tsx
+++ src/components/ui/progress.tsx
@@ -4,7 +4,7 @@ import * as React from 'react'
 import * as ProgressPrimitive from '@radix-ui/react-progress'
 import { cva, type VariantProps } from 'class-variance-authority'
 
-import { cn } from '@/lib/utils'
+import { cn } from '@/lib'
 
 const progressVariants = cva('h-full w-full flex-1 bg-primary transition-all', {
   variants: {
