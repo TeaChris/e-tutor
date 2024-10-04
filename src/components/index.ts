bbbf6d316bbaad29aa67790729a2ec6c0906fc63
bermuda <olasunkanmiboluwatife110@gmail.com>
2024-10-04 17:57:32 +0100

    chore: exported modls

---
+++ src/components/index.ts
@@ -0,0 +1,35 @@
+/*
+ * ############################################################################### *
+ * Created Date: Fr Oct 2024                                                   *
+ * Author: Boluwatife Olasunkanmi O.                                           *
+ * -----                                                                       *
+ * Last Modified: Fri Oct 04 2024                                              *
+ * Modified By: Boluwatife Olasunkanmi O.                                      *
+ * -----                                                                       *
+ * HISTORY:                                                                    *
+ * Date      	By	Comments                                                   *
+ * ############################################################################### *
+ */
+export * from './ui'
+export * from './Icon'
+export * from './Banner'
+export * from './Footer'
+export * from './Editor'
+export * from './modals'
+export * from './Navbar'
+export * from './Preview'
+export * from './MobileNav'
+export * from './info-card'
+export * from './NavRoutes'
+export * from './IconBadge'
+export * from './Categories'
+export * from './FileUpload'
+export * from './CourseCard'
+export * from './CourseLists'
+export * from './student-nav'
+export * from './SearchInput'
+export * from './CoursesList'
+export * from './CategoryItem'
+export * from './CourseProgress'
+export * from './MaxWidthWrapper'
+export * from './CourseEnrollButton'
