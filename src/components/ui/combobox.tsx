cdc54b7fb24c506af5cfe6baa5c6b1712c82ac04
bermuda <olasunkanmiboluwatife110@gmail.com>
2024-10-04 17:50:21 +0100

    chore: updated import

--- src/components/ui/combobox.tsx
+++ src/components/ui/combobox.tsx
@@ -3,20 +3,20 @@
 import * as React from 'react'
 import { Check, ChevronsUpDown } from 'lucide-react'
 
-import { cn } from '@/lib/utils'
-import { Button } from '@/components/ui/button'
+import { cn } from '@/lib'
+import { Button } from './button'
 import {
   Command,
   CommandEmpty,
   CommandGroup,
   CommandInput,
   CommandItem,
-} from '@/components/ui/command'
+} './command'
 import {
   Popover,
   PopoverContent,
   PopoverTrigger,
-} from '@/components/ui/popover'
+} from './popover'
 
 interface ComboboxProps {
   options: { label: string; value: string }[]
