ad7ba24decf34c8abb713df922dee7b5e2de3d3b
bermuda <olasunkanmiboluwatife110@gmail.com>
2024-10-04 18:02:26 +0100

    chore: exported files

---
+++ src/providers/index.ts
@@ -0,0 +1,15 @@
+/*
+ * ############################################################################### *
+ * Created Date: Fr Oct 2024                                                   *
+ * Author: Boluwatife Olasunkanmi O.                                           *
+ * -----                                                                       *
+ * Last Modified: Fri Oct 04 2024                                              *
+ * Modified By: Boluwatife Olasunkanmi O.                                      *
+ * -----                                                                       *
+ * HISTORY:                                                                    *
+ * Date      	By	Comments                                                   *
+ * ############################################################################### *
+ */
+
+export * from './ConfettiProvider'
+export * from './ToastProvider'
