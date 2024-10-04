e1997b887caa496b349035d4f3e5797d68b6a218
bermuda <olasunkanmiboluwatife110@gmail.com>
2024-10-04 17:52:09 +0100

    chore: updated import

---
+++ src/lib/index.ts
@@ -0,0 +1,19 @@
+/*
+ * ############################################################################### *
+ * Created Date: Fr Oct 2024                                                   *
+ * Author: Boluwatife Olasunkanmi O.                                           *
+ * -----                                                                       *
+ * Last Modified: Fri Oct 04 2024                                              *
+ * Modified By: Boluwatife Olasunkanmi O.                                      *
+ * -----                                                                       *
+ * HISTORY:                                                                    *
+ * Date      	By	Comments                                                   *
+ * ############################################################################### *
+ */
+export * from './db'
+export * from './utils'
+export * from './stripe'
+export * from './format'
+export * from './matadata'
+export * from './instructor'
+export * from './uploadthing'
