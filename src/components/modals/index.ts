5adf36c09678fe2cd84d36378ea0f0bd9ac91bde
bermuda <olasunkanmiboluwatife110@gmail.com>
2024-10-04 17:57:39 +0100

    exported components

---
+++ src/components/modals/index.ts
@@ -0,0 +1,13 @@
+/*
+ * ############################################################################### *
+ * Created Date: Fr Oct 2024                                                   *
+ * Author: Boluwatife Olasunkanmi O.                                           *
+ * -----                                                                       *
+ * Last Modified: Fri Oct 04 2024                                              *
+ * Modified By: Boluwatife Olasunkanmi O.                                      *
+ * -----                                                                       *
+ * HISTORY:                                                                    *
+ * Date      	By	Comments                                                   *
+ * ############################################################################### *
+ */
+export * from './ConfirmModal'
